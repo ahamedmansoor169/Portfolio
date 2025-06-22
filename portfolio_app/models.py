5b4378ec6062c057a370a8fb7bab86ffe895599d
Ahamed Mansoor M <ahamedmansoor169@gmail.com>
2025-06-23 00:09:06 +0530

    98% project completed, remaining projects section is there...

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+    email = models.EmailField(null=True,blank=True)
+    phone_number = models.CharField(max_length=15, null=True,blank=True)
+    service = models.CharField(max_length=250, null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from portfolio_app.models import Contact
+from django.contrib import messages
 
 def index(request):
     return render(request, "portfolio_app/index.html")
@@ -7,6 +9,22 @@ def about(request):
     return render(request, "portfolio_app/about.html")
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('conEmail')
+        phone_number = request.POST.get('conPhone')
+        service = request.POST.get('conService')
+        message = request.POST.get('conMessage')
+
+        conatct = Contact.objects.create(
+            name = name,
+            email = email,
+            phone_number = phone_number,
+            service = service,
+            message = message
+        )        
+        messages.success(request, 'Your message has been sent successfully!')
+        return redirect('contact')
     return render(request, "portfolio_app/contact.html")
 
 def projects(request):
