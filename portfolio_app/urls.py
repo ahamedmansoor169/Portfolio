2eac1182363caf4d3f4476dc5c772f8a85d492cf
Ahamed Mansoor M <ahamedmansoor169@gmail.com>
2025-06-23 18:53:53 +0530

    need to add some more projects

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path("contact/",views.contact, name="contact"),
     path("projects/",views.projects, name="projects"),
     path("services/",views.services, name="services"),
-    
-
 ]
