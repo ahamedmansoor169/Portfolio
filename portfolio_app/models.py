f6bf2ecca0180b7f84a09e860119835ae9caf8cd
Ahamed Mansoor M <ahamedmansoor169@gmail.com>
2025-07-27 22:21:25 +0530

    admin

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -7,3 +7,6 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=15, null=True,blank=True)
     service = models.CharField(max_length=250, null=True, blank=True)
     message = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
